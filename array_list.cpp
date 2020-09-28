a18a4a455422b6c59d1a4991d02f13e45af06f79
Stian Berg Hansen <stiabh@uio.no>
2020-09-28 13:04:41 +0200

    Completed 1c). Added methods append and resize

--- array_list.cpp
+++ array_list.cpp
@@ -8,6 +8,16 @@ private:
     int capacity = 1;   // actual size of array
     const int GROWTH_FACTOR = 2;
 
+    void resize() {
+        capacity = capacity * GROWTH_FACTOR;
+        int tmp* = new int[capacity];   // points to dynamic array
+        for (int i=0; i<size; i++) {
+            tmp[i] = data[i];
+        }
+        delete[] data;      // destroys old array
+        data = tmp;
+    }
+
 public:
     // constructor
     ArrayList() {
@@ -25,6 +35,13 @@ public:
         return size;
     }
 
+    void append(int n) {
+        if (size >= capacity) {
+            resize();
+        }
+        data[size] = n;
+        size += 1;
+    }
 };
 
 int main() {
