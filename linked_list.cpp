b3ca4f505a7e0af74546ad9f3c3dbc6c3f27a455
Stian Berg Hansen <stiabh@uio.no>
2020-09-29 10:31:44 +0200

    Completed 2b). Added LinkedList class with method length

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
 using namespace std;
 
+
 struct Node {
     int value;
-    Node* previous;
+    Node* prev;
     Node* next;
-}
+
+    Node(int v) {
+        value = v;
+        prev = nullptr;
+        next = nullptr;
+    }
+};
+
+
+class LinkedList {
+private:
+    Node* head;
+    Node* tail;
+
+public:
+    LinkedList() {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    int length() {
+        int length = 0;
+        Node* current = head;
+
+        while (current != nullptr) {
+            length += 1;
+            current = current->next;
+        }
+        return length;
+    }
+};
+
 
 int main() {
     return 0;
